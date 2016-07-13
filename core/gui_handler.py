1e6ad580308240c65037e521ecd8f559052c1d36
twanh <huiskenstwan@gmail.com>
2016-07-13 17:58:02 +0200

    Added more functions

--- core/gui_handler.py
+++ core/gui_handler.py
@@ -5,11 +5,35 @@ import sys
 
 class Handler(object):
     def __init__(self):
+
+        self.inp = ''
+
+        self.USER_NAME = 'Unknown'
+
+        self.user_prefix = '<h3 style="color:#3E6BC6;">[' + self.USER_NAME + ']> </h3>'
+        self.bot_prefix = '<h3 style="color:#4EC909;">[Darlene]> </h3>'
         self.app = QtGui.QApplication(sys.argv)
         self.MainWindow = QtGui.QMainWindow()
-        self.GUI = main_window.MainWindow()
+        self.MainGUI = main_window.MainWindow()
+        self.MainGUI.setup(self.MainWindow)
+        self.MainGUI.sendBtn.clicked.connect(self.user_inp)
+        self.MainGUI.messageBox.returnPressed.connect(self.MainGUI.sendBtn.click)
+
+        self.chatBoxCursor = QtGui.QTextCursor(self.MainGUI.chatBox.document())
 
     def run(self):
-        self.GUI.setup(self.MainWindow)
         self.MainWindow.show()
         sys.exit(self.app.exec_())
+
+    def user_inp(self):
+        self.inp = self.MainGUI.messageBox.text()
+        self.MainGUI.messageBox.clear()
+
+    def print_user(self, text):
+        self.chatBoxCursor.insertHtml(self.user_prefix)
+        self.chatBoxCursor.insertHtml(text + '<br>')
+
+    def print_bot(self, text):
+        self.chatBoxCursor.insertHtml(self.bot_prefix)
+        self.chatBoxCursor.insertHtml(text + '<br>')
+
